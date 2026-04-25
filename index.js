d2abfed07b5c84a0379e00687021e8851c9c429e
Prateek02AnandJha <jhaprateek234@gmail.com>
2026-04-25 18:55:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT ?? 8080;
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: 'Hello from the Server'
+    });
+}
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}   );
